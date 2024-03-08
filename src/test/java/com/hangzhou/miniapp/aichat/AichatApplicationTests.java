cfb754a768982e04f87b33e9381c5532f0ccfe22
wupan <wupan126@126.com>
2024-03-08 20:25:04 +0800

    init

---
+++ src/test/java/com/hangzhou/miniapp/aichat/AichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangzhou.miniapp.aichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
